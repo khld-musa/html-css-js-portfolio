e4e7ef25e8c513f8cd92cffa1acbf38cb68c76f2
khld-musa <elex22.sust@gmail.com>
2023-01-11 11:07:52 +0200

    contact form update-4

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
- 
+  
   $contact->smtp = array(
-    'host' => 'smtp.gmail.com',
+    'host' => 'smtp.gmail.com', https://flourishing-hotteok-3397aa.netlify.app/
     'username' => 'elex22.sust@gmail.com',
     'password' => 'mutgab11',
     'port' => '587'
   );
-  
+
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
